aca329c9b702c069dbe6a33d6b2fa6111fbcfe19
Alexandre Queiroz <alexandrepontesgyn@gmail.com>
2022-11-26 14:39:59 -0300

    feature: project's initial configuration with docker-compose to paises service

---
+++ paises/src/test/java/com/infnet/paises/PaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.paises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
