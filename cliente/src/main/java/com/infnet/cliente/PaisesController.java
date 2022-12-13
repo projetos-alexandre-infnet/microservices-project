893b2f49529cf5511b89a50c5b2bdaf0ac05859b
apontes77 <alexandrepontesgyn@gmail.com>
2022-12-12 21:54:23 -0300

    feature: adiciona logs na aplicação

--- cliente/src/main/java/com/infnet/cliente/PaisesController.java
+++ cliente/src/main/java/com/infnet/cliente/PaisesController.java
@@ -1,5 +1,7 @@
 package com.infnet.cliente;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,17 @@ public class PaisesController{
 
     private Client client;
 
+    private static Logger log = (Logger) LoggerFactory.getLogger(PaisesController.class);
+
     public PaisesController(Client client) {
         this.client = client;
     }
 
     @GetMapping("/phone-code")
     public ResponseEntity<String> getPhoneCodeByCountryName(@RequestParam("name") String name) {
-        return client.getCountry(name);
+        log.info("==>>> obtendo codigo do pais: {}", name);
+        ResponseEntity<String> country = client.getCountry(name);
+        log.info("-->> codigo obtido: {}", country.getBody());
+        return country ;
     }
 }
--- paises/src/main/java/com/infnet/paises/web/CountryController.java
+++ paises/src/main/java/com/infnet/paises/web/CountryController.java
@@ -1,6 +1,9 @@
 package com.infnet.paises.web;
 
 import com.infnet.paises.model.service.CountryService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/v1")
 public class CountryController {
 
     private CountryService service;
+
+    private static Logger log = (Logger) LoggerFactory.getLogger(CountryController.class);
 
     public CountryController(CountryService service) {
         this.service = service;
@@ -21,6 +27,7 @@ public class CountryController {
     @GetMapping("/countries")
     public ResponseEntity<String> findCodeByCountryName(@RequestParam("name") String name) {
         String founded = service.findByCountryName(name);
+        log.info("==>>> obtendo codigo: {}", founded);
         return new ResponseEntity<>(founded, HttpStatus.OK);
     }
 }
